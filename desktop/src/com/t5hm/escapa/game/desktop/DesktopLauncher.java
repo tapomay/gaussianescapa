6c720a79dc64f26febc0c9235f62c4ab345a99bf
tapomay.dey <tapomay.dey@gmail.com>
2014-09-11 06:34:48 +0530

    DesktopLauncher width height setting...equivalent to device size.

--- desktop/src/com/t5hm/escapa/game/desktop/DesktopLauncher.java
+++ desktop/src/com/t5hm/escapa/game/desktop/DesktopLauncher.java
@@ -9,6 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "GaussianEscapa";
+        config.width = 720;
+        config.height = 360;
         config.useGL30 = true;
 //        new LwjglApplication(new MainEscapaGame(), config);
         new LwjglApplication(new MainEscapaGame(), config);
